d8be8db1a0874394e621f6d204d9912615cabe74
Jakub Hlavac <Jakub.Hlavac@aimtecglobal.com>
2019-04-29 13:20:11 +0200

    Revert "not working pokus #záloha"
    This reverts commit 62dfcb21915f212647498b3bbf7d5071c04ee0ff.

--- envi-server/src/main/java/cz/aimtec/enviserver/controller/MeasurementRepository.java
+++ envi-server/src/main/java/cz/aimtec/enviserver/controller/MeasurementRepository.java
@@ -1,11 +1,10 @@
 package cz.aimtec.enviserver.controller;
 
-import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.CrudRepository;
 
 import cz.aimtec.enviserver.model.Measurement;
 
-public interface MeasurementRepository extends CrudRepository<Measurement, Long>, JpaSpecificationExecutor<Measurement> {
+public interface MeasurementRepository extends CrudRepository<Measurement, Long> {
 
 	public Iterable<Measurement> findBySensorUUID(String UUID);
 	
