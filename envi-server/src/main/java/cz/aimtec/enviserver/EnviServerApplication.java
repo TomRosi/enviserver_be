72bf2fbe97efd823e1d20e441cd32315850a4779
Jakub Hlavac <Jakub.Hlavac@aimtecglobal.com>
2019-12-03 16:27:48 +0100

    Mělo by jít nasadit do AWS bodem 11. v návodu

--- envi-server/src/main/java/cz/aimtec/enviserver/EnviServerApplication.java
+++ envi-server/src/main/java/cz/aimtec/enviserver/EnviServerApplication.java
@@ -1,12 +1,23 @@
 package cz.aimtec.enviserver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication(scanBasePackages={"cz.aimtec.enviserver"}) // same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class EnviServerApplication {
+public class EnviServerApplication extends SpringBootServletInitializer {
 
     
 	public static void main(String[] args) {				
@@ -15,4 +26,40 @@ public class EnviServerApplication {
 		
 		SpringApplication.run(EnviServerApplication.class, args);
 	}
+	
+	/*
+     * Create required HandlerMapping, to avoid several default HandlerMapping instances being created
+     */
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    /*
+     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+     */
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
+    /*
+     * optimization - avoids creating default exception resolvers; not required as the serverless container handles
+     * all exceptions
+     *
+     * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object, including
+     * an expensive ObjectMapper instance.
+     *
+     * To enable custom @ControllerAdvice classes remove this bean.
+     */
+    @Bean
+    public HandlerExceptionResolver handlerExceptionResolver() {
+        return new HandlerExceptionResolver() {
+
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+                return null;
+            }
+        };
+    }
 }
