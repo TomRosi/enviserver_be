546420b6e9aa81cdeb354577b89236ee520cba37
Tomas Rozsypal <Tomas.Rozsypal@aimtecglobal.com>
2019-12-16 10:07:10 +0100

    Vytvoření nové tabulky SensorTable a backendu k její obsluze

---
+++ envi-server/src/main/java/cz/aimtec/enviserver/model/SensorTable.java
@@ -0,0 +1,123 @@
+package cz.aimtec.enviserver.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="sensors", schema="dbo")
+@EnableTransactionManagement
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
+public class SensorTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @JsonIgnore
+    @Column(name = "uuid")
+    private String sensorUUID;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "min_temperature")
+    private Float minTemperature;
+
+    @Column(name = "max_temperature")
+    private Float maxTemperature;
+
+    public SensorTable() {
+    }
+
+    public SensorTable(String sensorUUID, String name, String location, Float minTemperature, Float maxTemperature) {
+        setSensorUUID(sensorUUID);
+        setName(name);
+        setLocation(location);
+        setMinTemperature(minTemperature);
+        setMaxTemperature(maxTemperature);
+    }
+
+    public void updateFrom(SensorTable measurement, boolean masterKey) {
+        if (measurement.getSensorUUID() != null && masterKey == true) {
+            setSensorUUID(measurement.getSensorUUID());
+        }
+        if (measurement.getName() != null) {
+            setName(measurement.getName());
+        }
+        if (measurement.getLocation() != null) {
+            setLocation(measurement.getLocation());
+        }
+        if (measurement.getMinTemperature() != null) {
+            setMinTemperature(measurement.getMinTemperature());
+        }
+        if (measurement.getMaxTemperature() != null) {
+            setMaxTemperature(measurement.getMaxTemperature());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SensorTable)) return false;
+        return id != null && id.equals(((SensorTable) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSensorUUID() {
+        return sensorUUID;
+    }
+
+    public void setSensorUUID(String sensorUUID) {
+        this.sensorUUID = sensorUUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Float getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(Float minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public Float getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(Float maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+}
